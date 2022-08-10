4e71e16dbc8fbff2b31f165fb2e88249491f7de5
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2022-08-09 21:36:32 -0300

    [feature] screen signup desktop layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./styles/reset.css";
 import "./styles/styles.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./containers/SignUp";
 
 export default function App() {
   return (
     <div className="App">
-      <p>Testing new react app</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element={<SignUp/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
