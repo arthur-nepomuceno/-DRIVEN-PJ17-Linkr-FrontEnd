5b83b6fc9ce66a90f4fd7720e64d8c18b3baded0
Lucas Palharini <lucas230991@hotmail.com>
2022-08-10 12:36:40 -0300

    feat: custom hook useLocalStorage

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+function useLocalStorage(key, initial) {
+
+    //setValue is a function return either the initial value or value stored in localstorage
+    const [value, setValue] = useState(() => {
+
+        const storedValue = localStorage.getItem(key);
+
+        return storedValue ? JSON.parse(storedValue) : initial;
+
+    });
+
+    useEffect(() => {
+
+        localStorage.setItem(key, JSON.stringify(value));
+        
+    }, [key, value]); //updates when key or value changes
+
+    return [value, setValue];
+}
+
+export default useLocalStorage;
+
+/**     INSTRUÇÃO DE USO
+ * 
+ *  Na página que quiserem usar esse custom hook criem a seguinte const:
+ *  const [localStorage, setLocalStorage] = useLocalStorage('NOME DA KEY DE ARMAZENAMENTO', VALOR PARA ARMAZENAR)
+ *  o valor armazenado pode ser objeto.
+ * 
+ *  v.1.0.0
+ **/
