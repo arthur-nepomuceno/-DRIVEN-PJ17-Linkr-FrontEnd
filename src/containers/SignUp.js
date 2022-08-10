7a583a5cc34c3d0d52e33d1bcdad761600ea43ae
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2022-08-10 14:36:34 -0300

    [feature] Screen signUp - Layout mobile

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -62,10 +62,12 @@ const Container = styled.div`
         position: absolute;
         top: 41%;
         left: 16%;
+        z-index: 1;
     }
 
     div#content {
         width: 37%;
+        height: 100vh;
         background-color: #333333;
         position: relative;
         display: flex;
@@ -128,6 +130,7 @@ const Container = styled.div`
         font-style: normal;
         font-weight: 400;
         font-size: 20px;
+        text-align: center;
         text-decoration-line: underline;
         color: #FFFFFF;
         position: absolute;
@@ -164,6 +167,110 @@ const Container = styled.div`
         button {
             font-size: 20px;
         }
+    }
+
+    @media (max-width: 1080px){
+        flex-direction: column;
+        width:  720px;
+
+        div#title {
+            width: 100%;
+            height: 175px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        h1 {
+            width: 45%;
+            height: fit-content;
+            top: 10px;
+            left: 28%;
+            font-size: 84px;
+            text-align: center;
+        }
+
+        h2 {
+            width: 40%;
+            height: fit-content;
+            top: 80px;
+            left: 30%;
+            font-size: 27px;
+            line-height: 38px;
+            text-align: center;
+        }
+
+        div#content {
+            width: 100%;
+        }
+
+        div#content div {
+            top: 40px;
+        }
+
+        p {
+            top: 440px;
+        }
+
+    }
+
+
+
+    @media (max-width: 720px){
+        flex-direction: column;
+        width:  375px;
+
+        div#title {
+            width: 100%;
+            height: 175px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        h1 {
+            width: 45%;
+            height: fit-content;
+            top: 10px;
+            left: 28%;
+            font-size: 76px;
+            text-align: center;
+        }
+
+        h2 {
+            width: 64%;
+            height: fit-content;
+            top: 80px;
+            left: 18%;
+            font-size: 23px;
+            line-height: 34px;
+            text-align: center;
+        }
+
+        div#content {
+            width: 100%;
+        }
+
+        div#content div {
+            top: 40px;
+        }
+
+        input, button {
+            width: 88%;
+            height: 55px;
+            font-size: 22px;
+        }
+
+        input::placeholder {
+            font-size: 22px;
+        }
+
+        p {
+            width: 41%;
+            top: 380px;
+            font-size: 17px;
+        }
+
     }
 `
 
